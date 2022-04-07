32d8497382675640b62ee9746e9a0aed3ccfacf0
Rafael Treiber <rafaeltreiber@yahoo.com.br>
2022-04-07 20:27:06 -0300

    added receive hit animation

--- index.js
+++ index.js
@@ -66,6 +66,10 @@ const player = new Fighter({
       imageSrc: "./img/samuraiMack/Attack1.png",
       framesMax: 6,
     },
+    takeHit: {
+      imageSrc: "./img/samuraiMack/Take Hit - white silhouette.png",
+      framesMax: 4,
+    },
   },
   attackBox: {
     offset: {
@@ -118,6 +122,10 @@ const enemy = new Fighter({
     attack1: {
       imageSrc: "./img/kenji/Attack1.png",
       framesMax: 4,
+    },
+    takeHit: {
+      imageSrc: "./img/kenji/Take hit.png",
+      framesMax: 3,
     },
   },
   attackBox: {
@@ -202,7 +210,7 @@ function animate() {
     enemy.switchSprite("fall");
   }
 
-  // detect for collision
+  // detect for collision and enemy hit
   if (
     rectangularCollision({
       rectangle1: player,
@@ -211,8 +219,9 @@ function animate() {
     player.isAttacking &&
     player.framesCurrent === 4
   ) {
+    enemy.takeHit();
     player.isAttacking = false;
-    enemy.health -= 20;
+
     document.querySelector("#enemyHealth").style.width = enemy.health + "%";
   }
 
@@ -221,6 +230,7 @@ function animate() {
     player.isAttacking = false;
   }
 
+  //this is where our player gets hit
   if (
     rectangularCollision({
       rectangle1: enemy,
@@ -229,8 +239,8 @@ function animate() {
     enemy.isAttacking &&
     enemy.framesCurrent === 2
   ) {
+    player.takeHit();
     enemy.isAttacking = false;
-    player.health -= 20;
     document.querySelector("#playerHealth").style.width = player.health + "%";
   }
 
